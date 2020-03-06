a8687db4293f6d3eac67f5dd0a7fbcd14ee8e656
Kevin Xiao <hxiao@us.ibm.com>
2020-03-05 18:33:04 -0600

    - cicd changes - tested managed logging deploy ok

---
+++ deploy/scripts/undeploy_managed.sh
@@ -0,0 +1,1 @@
+kubectl delete elasticstack.elasticstack.ibm.com/logging -n kube-system
--- deploy/scripts/undeploy_operator.sh
+++ deploy/scripts/undeploy_operator.sh
@@ -1,4 +1,4 @@
-kubectl delete -f deploy/operator.yaml
-kubectl delete -f deploy/role_binding.yaml
-kubectl delete -f deploy/role.yaml
-kubectl delete -f deploy/service_account.yaml
+kubectl delete -f deploy/operator.yaml -n ibm-elastic-stack-operator
+kubectl delete -f deploy/role_binding.yaml -n ibm-elastic-stack-operator
+kubectl delete -f deploy/role.yaml -n ibm-elastic-stack-operator
+kubectl delete -f deploy/service_account.yaml -n ibm-elastic-stack-operator

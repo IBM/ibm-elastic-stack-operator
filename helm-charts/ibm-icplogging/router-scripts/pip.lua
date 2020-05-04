62f750694afbf93483385a8fdb597e4866606e16
dian-xh <dian.xh@gmail.com>
2020-05-04 18:17:35 -0500

    backports fixes to support icp, kibana init issue 37303, 37572 (#14)
    * - fixes 37303
    - backports fixes to support icp
    
    * - fixes 37303
    - backports fixes to support icp
    
    * - fixes 37572
    - locally tested
    
    * - fixes
    
    Co-authored-by: Kevin Xiao <hxiao@us.ibm.com>

--- helm-charts/ibm-icplogging/router-scripts/pip.lua
+++ helm-charts/ibm-icplogging/router-scripts/pip.lua
@@ -13,7 +13,7 @@ local http = require "lib.resty.http"
 function PIP:query_user_id(token)
     local httpc = http.new()
     local res, err = httpc:request_uri(
-        "https://platform-identity-provider.ibm-common-services.svc."..self.cluster_domain..":4300/v1/auth/userInfo", 
+        "https://platform-identity-provider.{{ .Release.Namespace }}.svc."..self.cluster_domain..":4300/v1/auth/userInfo", 
         {
             method = "POST",
             ssl_verify = false,

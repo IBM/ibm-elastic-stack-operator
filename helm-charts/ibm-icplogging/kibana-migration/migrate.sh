9213c1ad88c011729088a81f8a65cb63dead6b18
Kevin Xiao <hxiao@us.ibm.com>
2020-07-30 17:37:01 -0500

    - fixed fresh install use case - fixed elk6 to elk6 use case

--- helm-charts/ibm-icplogging/kibana-migration/migrate.sh
+++ helm-charts/ibm-icplogging/kibana-migration/migrate.sh
@@ -13,12 +13,10 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-
 {{ template "elk.settings.all.prepare" . -}}
-
 set -uo pipefail
 
-echo "`date` - 1. preparing to migrate .kibana index"
+echo "`date` - 1. preparing to check .kibana index migration"
 # Prepare es client api url
 {{- if .Values.elasticsearch.security.authc.enabled }}
 # Use https protcol if security enabled
@@ -33,30 +31,55 @@ endpoint={{ .Values.elasticsearch.name }}
 port={{ .Values.elasticsearch.client.restPort }}
 url="$protocol://$endpoint:$port"
 
+# Check if the .kibana index migration is done
 # Check and wait until elasticsearch is up and sends back good json response
-exit_code="-1"
-
-while [ "$exit_code" != "0" ]
+while true
 do
-    echo "`date` - 2. looking for current .kibana index version"
+    echo "`date` - 2. checking if Elasticsearch is up"
+    temp_file=$(mktemp)
     set -x
-    resp_settings=$(curl -E $CERT_DIR/curator.crt --key $CERT_DIR/curator.key --cacert $CERT_DIR/ca.crt --fail $url/.kibana/_settings)
-    exit_code=$?
+    http_code=$(curl -o $temp_file -w '%{http_code}' -E $CERT_DIR/curator.crt --key $CERT_DIR/curator.key --cacert $CERT_DIR/ca.crt $url)
     set +x
 
-    echo "exit_code=$exit_code, resp_settings=$resp_settings"
+    resp=$(cat ${temp_file})
+    rm ${temp_file}
 
-    if [ "$exit_code" == "0" ]; then
-        echo "got 2xx http status code, checking response json..."
+    echo "http_code=$http_code, resp=$resp"
 
-        echo "`date` - 3. identifying kibana version"
-        created_by=$(echo $resp_settings| jq -r 'first(.[]).settings.index.version.created')
+    if [ "$http_code" != "200" ]; then
+        echo "Elasticsearch is not up yet... sleep 10s"
+        sleep 10
+        continue
+    fi
+
+    echo "`date` - 3. looking for current .kibana index version"
+    temp_file=$(mktemp)
+    set -x
+    http_code=$(curl -o $temp_file -w '%{http_code}' -E $CERT_DIR/curator.crt --key $CERT_DIR/curator.key --cacert $CERT_DIR/ca.crt $url/.kibana/_settings)
+    set +x
+
+    resp=$(cat ${temp_file})
+    rm ${temp_file}
+
+    echo "http_code=$http_code, resp=$resp"
+
+    case $http_code in
+    "404")
+        echo "no .kibana index found. use this as an indicator of a fresh install"
+        echo "no need to wait for migration"
+        break
+        ;;
+    "200")
+        echo "got 200 http status code, checking response json..."
+        echo "`date` - 4. identifying kibana version"
+        created_by=$(echo $resp| jq -r 'first(.[]).settings.index.version.created')
+
         if [[ -z "$created_by" ]]; then
             echo "no .kibana index found. use this as an indicator of a fresh install"
             created_by='6081099'
         fi
 
-        echo "index version=$created_by"
+        echo "$created_by=$created_by"
 
         major_version="${created_by:0:1}"
         echo "major_version=$major_version"
@@ -65,7 +88,7 @@ do
             echo "index version up-to-date. no need to migrate"
             break
         else
-            echo "`date` - 4. migrating .kibana index from version $created_by..."
+            echo "`date` - 5. migrating .kibana index from version $created_by..."
 
             # prepare the json
             json1='{
@@ -369,10 +392,12 @@ do
             {{- end }}
             set +x
         fi
-    else
-        echo "got bad http response code $exit_code. retrying in 10s..."
+        ;;
+    *)
+        echo "got bad http response code $http_code. retrying in 10s..."
         sleep 10
-    fi     
+        ;;
+    esac
 done
 
-echo "`date` - 5. Migration is DONE!"
+echo "`date` - 6. Migration is DONE!"

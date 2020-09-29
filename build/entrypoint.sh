7ca6213dc8b2238d36ccd7832e095ee0125ef02f
Bindiya <bindiytm@in.ibm.com>
2020-09-29 00:53:16 -0700

    changes

--- build/entrypoint.sh
+++ build/entrypoint.sh
@@ -14,4 +14,4 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 /scripts/upgrade-cr.sh &
-/usr/local/bin/helm-operator run --watches-file=./watches.yaml
+#/usr/local/bin/helm-operator run --watches-file=./watches.yaml
